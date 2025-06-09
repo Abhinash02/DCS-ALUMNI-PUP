5cabd4ec4aeb41110b27e74123be496bd3c9faae
Kanikabhatia08 <abhinashabhi66100@gmail.com>
2025-06-09 16:06:01 +0530

    Removed comment out code

--- new-master/backend/utils/multer.js
+++ new-master/backend/utils/multer.js
@@ -1,34 +1,3 @@
-// const multer = require("multer");
-// const path = require("path");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-//   limits: { fileSize: 50 * 1024 * 1024 }, // 50MB limit
-//   fileFilter: (req, file, cb) => {
-//     const filetypes = /jpeg|jpg|png/;
-//     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-//     const mimetype = filetypes.test(file.mimetype);
-//     if (extname && mimetype) {
-//       return cb(null, true);
-//     }
-//     cb(new Error("Only images (jpeg, jpg, png) are allowed"));
-//   },
-// });
-
-// module.exports = upload;
-
-
-
-
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
